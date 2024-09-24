35077f2c835f6404a04ced43e50ad970144d2de4
Gavin <oiamo123@gmail.com>
2024-09-23 21:36:28 -0600

    Added config file and connection class for db

---
+++ src/main/java/org/example/demo/ConnectionDB.java
@@ -0,0 +1,40 @@
+package org.example.demo;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class ConnectionDB {
+    public static Connection getConnection() {
+        Connection conn = null;
+
+        String username;
+        String password;
+        String uri;
+
+        try {
+            InputStream file = TravelExpertsApplication.class.getResourceAsStream("config.properties");
+            Properties prop = new Properties();
+            prop.load(file);
+
+            username = prop.getProperty("username");
+            password = prop.getProperty("password");
+            uri = prop.getProperty("uri");
+
+            System.out.println(username);
+            System.out.println(password);
+            System.out.println(uri);
+        } catch (Exception err) {
+            throw new RuntimeException("Unable to read file");
+        }
+
+        try {
+            conn = DriverManager.getConnection(uri, username, password);
+        } catch (Exception err) {
+            throw new RuntimeException(err.getMessage());
+        }
+        return conn;
+    }
+}
--- src/main/java/org/example/demo/MainController.java
+++ src/main/java/org/example/demo/MainController.java
@@ -3,12 +3,20 @@ package org.example.demo;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+import java.sql.Connection;
+
+public class MainController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    private void initialize() {
+        Connection conn = ConnectionDB.getConnection();
+        System.out.println("Connected to db");
     }
 }
